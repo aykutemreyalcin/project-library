15c70756ee9053f48ad9c6333d7e2468c443a2a0
root <root@aykut.aykut>
2024-06-19 04:17:23 -0500

    making changes on classes

--- patron.py
+++ patron.py
@@ -1,15 +1,29 @@
 from main import Person
+from book import *
 
 class Patron(Person):
     def __init__(self,name,patron_id):
-        self.name = name
-        self.patron_id = patron_id
+        super().__init__(name,patron_id)
+        self.borrowed_books = []
 
     def __str__(self):
-        return f"patron name : {self.name} , patron id : {self.patron_id}"
+        borrowed_book_titles = ", ".join([i for i in self.borrowed_books])
+        return f"patron name : {self.name} , patron id : {self.patron_id} borrowed books : {borrowed_book_titles}"
 
     def borrow_book(self,library,book):
-        pass
+        if book.available:
+            self.borrowed_books.append(book)
+            book.available = False
+            print(f"{self.name} borrowed {book.title}")
+        else:
+            print(f"book {book.name} is not available")
 
     def return_book(self,book):
-        pass
+        if book in self.borrowed_books:
+            self.borrowed_books.remove(book)
+            book.available = True
+            print(f"{self.name} returned {book.title}")
+        else:
+            print(f"{book.title} is not at {self.name}")
+
+
